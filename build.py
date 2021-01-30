35af5002710ce8c7537bfca3b15add14616b55f6
Neelofar Tamboli <tamboli.neelofar@gmail.com>
2021-01-30 14:56:02 -0500

    fixed cythonize path and removed build.c and setup.c

--- build.py
+++ build.py
@@ -9,7 +9,9 @@ targetDirName = "myProj"
 currentWorkingDir = os.getcwd()
 buildDirName = targetDirName + "_build"
 buildDirPath = ''
-
+path_separator = '/'
+if os.name == 'nt':
+    path_separator = '\\'
 
 def createBuild():
     if os.path.exists(buildDirName) or os.path.exists("package"):
@@ -24,21 +26,26 @@ def createBuild():
 
             print("Deleted old directory.")
             createBuild()
+            print("After 2nd create build")
         elif dlPrevDir == 'N':
             print("Stopped.")
         else:
             print("Enter valid input.")
             createBuild()
     else:
-        buildDirPath = shutil.copytree(src=currentWorkingDir + "/" + targetDirName,
-                                       dst=currentWorkingDir + "/" + buildDirName)
+        print("Copying target directory to build directory")
+        print("Target: " + currentWorkingDir + path_separator + targetDirName)
+        print("Build: " + currentWorkingDir + path_separator + buildDirName)
+        buildDirPath = shutil.copytree(src=currentWorkingDir + path_separator + targetDirName,
+                                       dst=currentWorkingDir + path_separator + buildDirName)
         createSetupFile(buildDirPath)
         removePyFiles(buildDirPath)
 
 
 def createSetupFile(absolutBuildDirPath):
     f = open(os.path.join(currentWorkingDir, "setup.py"), "a+")
-    f.write("""\
+    print("setup file")
+    print("""\
 from Cython.Build import cythonize
 from distutils.core import setup
 from distutils.extension import Extension
@@ -46,13 +53,25 @@ from Cython.Distutils import build_ext
 import os
 
 setup(
-    cmdclass={'build_ext': build_ext},
-    name="remotepy", 
-    ext_modules=cythonize(\
-    \"""" + absolutBuildDirPath + """\" + '**/*.py'),)""")
+    cmdclass = {'build_ext': build_ext},
+    name = "mysqleasy",
+    ext_modules = cythonize('**/*.py'),
+)""")
+    f.write("""
+from Cython.Build import cythonize
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+import os
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    name = "mysqleasy",
+    ext_modules = cythonize('**/*.py'),
+)""")
     f.close()
     os.system(f"python setup.py build_ext --inplace")
-    os.remove(f"{currentWorkingDir}/setup.py")
+    os.remove(f"{currentWorkingDir}" + path_separator + "setup.py")
 
 
 def removePyFiles(buildDirPath):
@@ -73,21 +92,33 @@ def removePydFromRoot():
                 if file.endswith(".pyd") and dir != "package":
                     os.remove(os.path.join(root, file))
 
-
+print("Creating build ...")
 createBuild()
 
+if os.path.exists("build.c"):
+    os.remove("build.c")
+if os.path.exists("setup.c"):
+    os.remove("setup.c")
+
+print("Creating package directory ...")
+print(currentWorkingDir+ path_separator + "package")
 # creating package directory
 packageDirPath = shutil.copytree(src=currentWorkingDir,
-                                       dst=currentWorkingDir + "/package")
-
+                                       dst=currentWorkingDir + path_separator + "package")
+print("Removing  directory ... " +packageDirPath + path_separator + buildDirName)
 # remove files and folders from package
-shutil.rmtree(packageDirPath + "/" + buildDirName)
-shutil.rmtree(packageDirPath + "/" + targetDirName)
-os.remove(packageDirPath + "/build.py")
+shutil.rmtree(packageDirPath + path_separator + buildDirName)
+print("Removing  directory ... " +packageDirPath + path_separator + targetDirName)
+shutil.rmtree(packageDirPath + path_separator + targetDirName)
+print("Removing  file ... " +packageDirPath + path_separator + "build.py")
+os.remove(packageDirPath + path_separator + "build.py")
 
 # Delete build folder from root
-shutil.rmtree(currentWorkingDir + "/" + buildDirName)
-shutil.rmtree(currentWorkingDir + "/build")
+print("Removing directory ... " +currentWorkingDir + path_separator + buildDirName)
+shutil.rmtree(currentWorkingDir + path_separator + buildDirName)
+print("Removing directory ... " +currentWorkingDir + path_separator + "build")
+shutil.rmtree(currentWorkingDir + path_separator + "build")
 
+print("Removing pyd files ...")
 removePydFromRoot()
 
